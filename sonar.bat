9208f9231d3d3d15ed6e8a769752844c5e47aac4
equiman <equiman@users.noreply.github.com>
2018-06-21 16:33:17 -0500

    Add sonar cloud analysis

---
+++ sonar.bat
@@ -0,0 +1,8 @@
+@echo off
+set /p token=<sonar.txt
+:: https://docs.sonarqube.org/display/SONAR/Analysis+Parameters
+SonarQube.Scanner.MSBuild.exe begin /k:"dein:tddroman" /n:"Number" /v:"1.0.0" /o:"dein" /d:sonar.host.url="https://sonarcloud.io" /d:sonar.login="%token%" /d:sonar.language="cs" /d:sonar.exclusions="**/bin/**/*,**/obj/**/*" /d:sonar.coverage.exclusions="Number.Tests/**,**/*Tests.cs" /d:sonar.cs.opencover.reportsPaths="%cd%\lcov.opencover.xml"
+dotnet restore
+dotnet build
+dotnet test Number.Tests/Number.Tests.csproj /p:CollectCoverage=true /p:CoverletOutputFormat=\"opencover,lcov\" /p:CoverletOutput=../lcov
+SonarQube.Scanner.MSBuild.exe end /d:sonar.login="%token%"
