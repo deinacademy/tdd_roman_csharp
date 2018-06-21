95408c04a02f3f3ea0e3ac563c60974be95ec909
equiman <equiman@users.noreply.github.com>
2018-06-21 11:58:01 -0500

    Refactor with Place Values method, ones and tents

--- Number/Arabic.cs
+++ Number/Arabic.cs
@@ -7,34 +7,12 @@ namespace Number
         public string ConvertToRoman(int number)
         {
             string result = "";
-            if (number <= 3)
-            {
-                result = RepeatLiteral(number);
-            }
-            else if (number == 4)
-            {
-                result = "IV";
-            }
-            else if (number == 5)
-            {
-                result = "V";
-            }
-            else if (number >= 6 && number <= 8)
-            {
-                result = "V" + RepeatLiteral(number - 5);
-            }
-            else if (number == 9)
-            {
-                result = "IX";
-            }
-            else if (number == 10)
-            {
-                result = "X";
-            }
-            else
-            {
-                result = "XI";
-            }
+            int tens = number / 10 % 10;
+            int ones = number / 1 % 10;
+
+            result += PlaceValues(tens, new string[] { "X", "L", "C" });
+            result += PlaceValues(ones, new string[] { "I", "V", "X" });
+
             if (String.IsNullOrEmpty(result))
             {
                 throw new ArgumentException("Value can't be converted");
@@ -42,12 +20,38 @@ namespace Number
             return result;
         }
 
-        private static string RepeatLiteral(int times)
+        private static string PlaceValues(int number, string[] symbols)
+        {
+            string result = "";
+            if (number <= 3)
+            {
+                result = RepeatLiteral(number, symbols[0]);
+            }
+            else if (number == 4)
+            {
+                result = symbols[0] + symbols[1];
+            }
+            else if (number == 5)
+            {
+                result = symbols[1];
+            }
+            else if (number >= 6 && number <= 8)
+            {
+                result = symbols[1] + RepeatLiteral(number - 5, symbols[0]);
+            }
+            else if (number == 9)
+            {
+                result = symbols[0] + symbols[2];
+            }
+            return result;
+        }
+
+        private static string RepeatLiteral(int times, string symbol)
         {
             string result = "";
             for (int i = 0; i < times; i++)
             {
-                result += "I";
+                result += symbol;
             }
             return result;
         }
